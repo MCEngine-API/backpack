b148cba4dffe3d809d9024c3cdad36f85f8ad967
JetsadaWijit <jetsadawijit@outlook.com>
2024-12-28 10:48:58 +0000

    reset projec

--- mcengine-backpack-api/src/main/java/io/github/mcengine/api/MCEngineBackPackApi.java
+++ mcengine-backpack-api/src/main/java/io/github/mcengine/api/MCEngineBackPackApi.java
@@ -1,21 +1,3 @@
 package io.github.mcengine.api;
 
-import io.github.mcengine.api.MCEngineBackPackApiUtil;
-import org.bukkit.inventory.ItemStack;
-
-public class MCEngineBackPackApi {
-
-    private static MCEngineBackPackApiUtil mcengineBackPackApiUtil;
-
-    // Initializes the utility class (ensure this is called during plugin setup)
-    public static void initialize(MCEngineBackPackApiUtil apiUtil) {
-        mcengineBackPackApiUtil = apiUtil;
-    }
-
-    public static ItemStack getBackPack(String texture, int size) {
-        if (mcengineBackPackApiUtil == null) {
-            throw new IllegalStateException("MCEngineBackPackApiUtil is not initialized.");
-        }
-        return mcengineBackPackApiUtil.createBackpack(texture, size);
-    }
-}
+public class MCEngineBackPackApi {}
--- spigotmc/src/main/java/io/github/mcengine/MCEngineBackPack.java
+++ spigotmc/src/main/java/io/github/mcengine/MCEngineBackPack.java
@@ -1,23 +1,11 @@
 package io.github.mcengine;
 
-import io.github.mcengine.api.backpack.command.MCEngineBackPackApiCommand;
-import io.github.mcengine.api.backpack.listener.MCEngineBackPackApiListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MCEngineBackPack extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Register the "backpack" command and its executor
-        if (getCommand("backpack") != null) {
-            getCommand("backpack").setExecutor(new MCEngineBackPackApiCommand());
-        } else {
-            getLogger().warning("Command 'backpack' could not be found in plugin.yml!");
-        }
-
-        // Register event listeners
-        getServer().getPluginManager().registerEvents(new MCEngineBackPackApiListener(this), this);
-
         getLogger().info("MCEngineBackPack has been enabled.");
     }
 
