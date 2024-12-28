a4be5b93b736a1e32a4645a62ef12239c1824a16
JetsadaWijit <jetsadawijit@outlook.com>
2024-12-28 09:32:07 +0000

    Added createBackPack(String texture, int size)

--- mcengine-backpack-api/src/main/java/io/github/mcengine/api/MCEngineBackPackApi.java
+++ mcengine-backpack-api/src/main/java/io/github/mcengine/api/MCEngineBackPackApi.java
@@ -1,3 +1,11 @@
 package io.github.mcengine.api;
 
-class MCEngineBackPack {}
+import org.bukkit.inventory.ItemStack;
+
+class MCEngineBackPack {
+    private static MCEngineBackPackApiUtil mcengineBackPackApiUtil;
+
+    public ItemStack getBackPack(String texture, int size) {
+        return mcengineBackPackApiUtil.createBackpack();
+    }
+}
