240f18457fcf5bfb1f63cf35216061302ec32858
JetsadaWijit <jetsadawijit@outlook.com>
2025-04-14 17:30:12 +0000

    feat: set up simple springframework

---
+++ src/main/java/io/github/mcengine/app/backpack/BackpackApplication.java
@@ -0,0 +1,11 @@
+package io.github.mcengine.app.backpack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackpackApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BackpackApplication.class, args);
+    }
+}
---
+++ src/main/java/io/github/mcengine/app/backpack/controller/HomeController.java
@@ -0,0 +1,15 @@
+package io.github.mcengine.app.backpack.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("message", "Welcome to Backpack Website!");
+        return "index";
+    }
+}
